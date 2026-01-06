7cdc023bf3db6c564004de34dd04012ad2f50a19
saidbabaiev <babaiev.info@gmail.com>
2026-01-06 20:13:50 +0100

    refactor: remove redundant useSupabaseClient composable (using built-in from @nuxtjs/supabase)

--- app/pages/applications.vue
+++ app/pages/applications.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1 class="text-2xl font-bold">Applications page</h1>
+    <div class="h-full bg-cyan-200">
+        <h1 class="text-2xl font-bold">Applications page</h1>
+    </div>
 </template>
--- app/pages/clients.vue
+++ app/pages/clients.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1 class="text-2xl font-bold">Clients page</h1>
+    <div class="h-full bg-slate-200">
+        <h1 class="text-2xl font-bold">Clients page</h1>
+    </div>
 </template>
