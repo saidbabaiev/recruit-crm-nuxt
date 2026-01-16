c9069144f39e379130f6fa47fe6524daf4d069b7
saidbabaiev <babaiev.info@gmail.com>
2026-01-16 17:23:44 +0100

    fixed eslint settings, created vue-query plugin

---
+++ app/plugins/vue-query.ts
@@ -0,0 +1,24 @@
+import { VueQueryPlugin, QueryClient, type VueQueryPluginOptions } from '@tanstack/vue-query'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5000,
+        refetchOnWindowFocus: false,
+      },
+    },
+  })
+
+  const options: VueQueryPluginOptions = {
+    queryClient,
+  }
+
+  nuxtApp.vueApp.use(VueQueryPlugin, options)
+
+  return {
+    provide: {
+      queryClient,
+    },
+  }
+})
