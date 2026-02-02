4302c12cdbec0843956c2cc0fced2a940c2ae0a7
saidbabaiev <babaiev.info@gmail.com>
2026-02-02 12:40:35 +0100

    ui: implement basic form fields for add candidate (first name, last name, email, phone)

--- app/components/candidates/CandidateSheetForm.vue
+++ app/components/candidates/CandidateSheetForm.vue
@@ -6,6 +6,16 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet'
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field'
+
+import { useForm, Field as VeeField } from 'vee-validate'
+import { z } from 'zod'
 
 interface Props {
   open: boolean
@@ -21,6 +31,10 @@ const open = computed({
   get: () => props.open,
   set: value => emit('update:open', value),
 })
+
+const onSubmit = (values: unknown) => {
+  console.log(values)
+}
 </script>
 
 <template>
@@ -33,6 +47,110 @@ const open = computed({
           and remove your data from our servers.
         </SheetDescription>
       </SheetHeader>
+      <div class="p-4">
+        <form
+          id="candidate-form"
+          class="space-y-4"
+          @submit="onSubmit"
+        >
+          <FieldGroup class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <!-- First Name -->
+            <VeeField
+              v-slot="{ field, errors }"
+              name="first_name"
+            >
+              <Field :data-invalid="!!errors.length">
+                <FieldLabel for="candidate-first-name">
+                  First Name
+                </FieldLabel>
+                <Input
+                  id="candidate-first-name"
+                  v-bind="field"
+                  placeholder="John"
+                  :aria-invalid="!!errors.length"
+                />
+                <FieldError
+                  v-if="errors.length"
+                  :errors="errors"
+                />
+              </Field>
+            </VeeField>
+
+            <!-- Last Name -->
+            <VeeField
+              v-slot="{ field, errors }"
+              name="last_name"
+            >
+              <Field :data-invalid="!!errors.length">
+                <FieldLabel for="candidate-last-name">
+                  Last Name
+                </FieldLabel>
+                <Input
+                  id="candidate-last-name"
+                  v-bind="field"
+                  placeholder="Doe"
+                  :aria-invalid="!!errors.length"
+                />
+                <FieldError
+                  v-if="errors.length"
+                  :errors="errors"
+                />
+              </Field>
+            </VeeField>
+          </FieldGroup>
+
+          <FieldGroup>
+            <!-- Email -->
+            <VeeField
+              v-slot="{ field, errors }"
+              name="email"
+            >
+              <Field :data-invalid="!!errors.length">
+                <FieldLabel for="candidate-email">
+                  Email
+                </FieldLabel>
+                <Input
+                  id="candidate-email"
+                  v-bind="field"
+                  placeholder="john.doe@example.com"
+                  :aria-invalid="!!errors.length"
+                />
+                <FieldError
+                  v-if="errors.length"
+                  :errors="errors"
+                />
+              </Field>
+            </VeeField>
+
+            <!-- Phone -->
+            <VeeField
+              v-slot="{ field, errors }"
+              name="phone"
+            >
+              <Field :data-invalid="!!errors.length">
+                <FieldLabel for="candidate-phone">
+                  Phone
+                </FieldLabel>
+                <Input
+                  id="candidate-phone"
+                  v-bind="field"
+                  placeholder="1234567890"
+                  :aria-invalid="!!errors.length"
+                />
+                <FieldError
+                  v-if="errors.length"
+                  :errors="errors"
+                />
+              </Field>
+            </VeeField>
+          </FieldGroup>
+        </form>
+      </div>
+      <SheetFooter>
+        <Button type="submit">
+          Create
+        </Button>
+      </SheetFooter>
     </SheetContent>
   </Sheet>
 </template>
